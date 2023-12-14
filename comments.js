7f6412d7b8a71c6a163d52e5509a5d6437be20e6
FernandoD <fernandodanielp49@gmail.com>
2023-12-14 10:43:23 -0600

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,11 +1,82 @@
-//create server web 
-const express = require("express");
-const router = express.Router();
-const { getComments, createComment, deleteComment } = require("../controllers/comments");
-const { protect } = require("../middleware/auth");
+//create server web socket
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
-//router.get("/", getComments);
-router.post("/", protect, createComment);
-router.delete("/:id", protect, deleteComment);
+//create connection to database
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'root',
+  database : 'comments'
+});
 
-module.exports = router;
+//connect to database
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
+
+//create table
+connection.query("CREATE TABLE IF NOT EXISTS comments (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), comment VARCHAR(255))", function (err, result) {
+  if (err) throw err;
+  console.log("Table created");
+});
+
+// Path: comments.js
+//create server web socket
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+//create connection to database
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'root',
+  database : 'comments'
+});
+
+//connect to database
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
+
+//create table
+connection.query("CREATE TABLE IF NOT EXISTS comments (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), comment VARCHAR(255))", function (err, result) {
+  if (err) throw err;
+  console.log("Table created");
+});
+
+// Path: comments.js
+//create server web socket
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+//create connection to database
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'root',
+  database : 'comments'
+});
+
+//connect to database
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
+
+//create table
+connection.query("CREATE TABLE IF NOT EXISTS comments (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), comment VARCHAR(255))", function (err, result) {
+  if (err) throw err;
+  console.log("Table created");
+});
+
+// Path: comments.js
+//create server web socket
