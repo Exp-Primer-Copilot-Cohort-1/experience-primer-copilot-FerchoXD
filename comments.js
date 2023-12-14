13660fa47b7e16a166a31d0d3294624777e6e71e
FernandoD <fernandodanielp49@gmail.com>
2023-12-14 10:52:09 -0600

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,1 +1,49 @@
-//create server web
+//create server web socket
+var express = require('express');
+var router = express.Router();
+var mongoose = require('mongoose');
+var Comment = require('../models/Comment.js');
+var passport = require('passport');
+require('../config/passport')(passport);
+var jwt = require('jsonwebtoken');
+
+//GET request to return all comments
+router.get('/', function(req, res, next) {
+  Comment.find(function (err, comments) {
+    if (err) return next(err);
+    res.json(comments);
+  });
+});
+
+//GET request to return a comment by ID
+router.get('/:id', function(req, res, next) {
+  Comment.findById(req.params.id, function (err, post) {
+    if (err) return next(err);
+    res.json(post);
+  });
+});
+
+//POST request to create a new comment
+router.post('/', function(req, res, next) {
+  Comment.create(req.body, function (err, post) {
+    if (err) return next(err);
+    res.json(post);
+  });
+});
+
+//PUT request to update a comment by ID
+router.put('/:id', function(req, res, next) {
+  Comment.findByIdAndUpdate(req.params.id, req.body, function (err, post) {
+    if (err) return next(err);
+    res.json(post);
+  });
+});
+
+//DELETE request to delete a comment by ID
+router.delete('/:id', function(req, res, next) {
+  Comment.findByIdAndRemove(req.params.id, req.body, function (err, post) {
+    if (err) return next(err);
+    res.json(post);
+  });
+});
+module.exports = router;
